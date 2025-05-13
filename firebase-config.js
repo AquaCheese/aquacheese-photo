de124442b809683731792e708f6b558863cff41b
AquaCheese <aquacheese.yt@gmail.com>
2025-05-13 08:57:17 +0100

    secret update

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD-7v1x4X2g5k3J6Q8q0Z9G4z5Y1F3a2cE",
+  authDomain: "aquacheese-photo-website.firebaseapp.com",
+  projectId: "aquacheese-photo-website",
+  storageBucket: "aquacheese-photo-website.appspot.com",
+  messagingSenderId: "637266954240",
+  appId: "1:637266954240:web:6e55076f10c27ad4c230e5"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
