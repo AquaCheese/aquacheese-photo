5834ae5d661c05042a411e13a84e7864a8e8f499
AquaCheese <aquacheese.yt@gmail.com>
2025-05-16 16:11:32 +0100

    hehe
    transferring files ig

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+window.firebaseConfig = {
+  apiKey: "AIzaSyDKe84a_3xgi3xv3DRkRTk6cXyNbCtX-Wo",
+  authDomain: "aquacheese-photography-site.firebaseapp.com",
+  projectId: "aquacheese-photography-site",
+  storageBucket: "aquacheese-photography-site.appspot.com",
+  messagingSenderId: "671895809880",
+  appId: "1:671895809880:web:4ee9031d2041f83ae57f2d",
+  measurementId: "G-EJKMML5E3K"
+};
